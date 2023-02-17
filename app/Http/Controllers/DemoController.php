a041ccfc58b5f147b077d86eef5fa1a69f8f4871
DuyRom <duyit192@hotmail.com>
2023-02-17 15:14:06 +0700

    create fnDemo

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -13,24 +13,9 @@ class DemoController extends Controller
     		'id'=>1,
     		'name'=>'demo'
     	];
-    	// $pdf = Pdf::loadView('pdf.invoice', $data);
-    	// // dd($pdf);
-    	// return $pdf->download('invoice.pdf');
-    	// $path = '/pdf/usec.pdf';
-    	// $pdf = \App::make('dompdf.wrapper');
-    	// $pdf->loadHTML(public_path().'/pdf/usec.pdf');
-    	// return $pdf->stream();
-    	//return Pdf::loadFile(public_path().'/pdf/usec.pdf')->stream('download.pdf');
-        // modifile
-    	return Pdf::loadFile(public_path().'/storage/fonts/1.pdf')->stream('download.pdf');
-  //   	$customer = Customer::findOrFail($id);
-		// $pdf = Pdf::loadView('pdf.invoice', $data);
-		// return $pdf->stream('info.pdf', array("Attachment" => false));
-		// return $pdf->stream('info.pdf
     }	
-
-    public function checkoutFromDm($value='')
-    {
-        # code...
-    }
+     public function fnDemo($value='')
+     {
+         # code...
+     }
 }
