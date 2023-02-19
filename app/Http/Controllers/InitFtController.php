715955be53bb2251e2d80b58ac92f03a60510a08
DuyRom <duyit192@hotmail.com>
2023-02-19 20:21:09 +0700

    make InitFtController

---
+++ app/Http/Controllers/InitFtController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InitFtController extends Controller
+{
+    public function index($value='')
+    {
+        // code...
+    }
+}
